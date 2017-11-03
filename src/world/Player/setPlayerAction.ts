69f7ceb59ab0525cea503f18980e7e431cff936c
hejny <me@pavolhejny.com>
2017-11-04 00:39:00 +0100

    + Throwing bricks

--- src/world/Player/setPlayerAction.ts
+++ src/world/Player/setPlayerAction.ts
@@ -8,12 +8,12 @@ export default function setPlayerAction(
     const onPointerDown = ()=>{
         new Brick(
             player.world,
-            'wood-fence',
-            {mass:1000,restitution:0.5},
+            'clay-bricks',
+            {mass:5000,restitution:0.5},
             new BABYLON.Vector3(2,2,2),
             player.mesh.position.add(player.direction1),
             BABYLON.Vector3.Zero(),
-            player.direction1.scale(100).add(new BABYLON.Vector3(0,10,0)),
+            player.direction1.scale(100).add(new BABYLON.Vector3(0,30,0)),
             new BABYLON.Vector3(
                 (Math.random()-.5)*Math.PI*10,
                 (Math.random()-.5)*Math.PI*10,
